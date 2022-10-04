e5f9f3dc88fca4c5e04ed507dff536906bb9bf76
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-10-04 22:45:45 +0300

    finish

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -3,7 +3,6 @@ const { Contact } = require("../../models");
 const addContact = async (req, res) => {
   const result = await Contact.create(req.body);
   res.status(201).json(result);
-  console.log(1);
 };
 
 module.exports = addContact;
