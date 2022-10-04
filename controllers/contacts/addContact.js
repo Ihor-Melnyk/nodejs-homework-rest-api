fbac691fb6484916b00e004fd46d1eed13c2e382
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-10-04 22:45:11 +0300

    newBranche

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -3,6 +3,7 @@ const { Contact } = require("../../models");
 const addContact = async (req, res) => {
   const result = await Contact.create(req.body);
   res.status(201).json(result);
+  console.log(1);
 };
 
 module.exports = addContact;
